cc0ceaf9160751360324a4c91569cdce30f80474
marielklem <mariel.steinbeiser@gmail.com>
2019-04-25 18:45:07 -0400

    outlined basic routes

--- models/users.js
+++ models/users.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const productSchema = require('./products').schema
 
+//add intake form to user's profile
+
 const UserSchema = new Schema ({
   name: String,
   email: String,
   image: String,
+  location: String,
+  accountType: String,
   cart: [{ type: Schema.Types.ObjectId, ref: 'Product' }]
 })
 
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const User = require('../models/users')
+
+// Find the id of the user
+
+// Post login (/login)
+
+// Logout (/logout)
+
+// Get user profile information (intake form)
+
+// Post user profile information (new user)
+
+// Put user profile information (update user)
+
+// Get all items in user's cart
+
+// Post a product to user's cart
+
+// Delete item from cart
+
+// 
