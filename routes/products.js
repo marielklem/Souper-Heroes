55ab2662bc3a294051ded0eecc41378a47649949
marielklem <mariel.steinbeiser@gmail.com>
2019-04-25 17:51:06 -0400

    first commit

---
+++ routes/products.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Product = require('../models/products')
+
+
+//Find ID of selected product
+
+//get all products ()
+
+//post a single product (for admin to update inventory quantity)
+
+//post products (for admin to add new inventory)
+
