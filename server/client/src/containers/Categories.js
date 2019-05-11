1cca626746e6f61022990cb9e9fda6ddc3304aab
marielklem <mariel.steinbeiser@gmail.com>
2019-05-11 11:15:46 -0400

    updated categories to update with click

--- server/client/src/containers/Categories.js
+++ server/client/src/containers/Categories.js
@@ -3,16 +3,33 @@ import { connect } from 'react-redux';
 import * as actionTypes from "../actions";
 import Products from "./Products"
 
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
 class Categories extends Component {
+  state = {
+    color: "green"
+  }
   //on page load, fetch all products
   componentDidMount() {
     this.props.fetchCategories();
     this.props.fetchProducts();
-    
+  }
+
+  updateTotal = (category, total) => {
+    this.setState({[category]: total}, () => {
+      const category1 = this.props.categories.find( (limit) => {
+        return limit.name === category
+      })
+      if (this.state[category] >= category1.limit) {
+        this.setState({color: "red"})
+      } else {
+        this.setState({color: "green"})
+      }
+    })
   }
 
   //for each category, add all matching products
-  addProducts(category) {
+  renderProducts(category) {
     if (this.props.inventory.length === 0) {
       return(<div>...loading</div>)
     }
@@ -22,7 +39,7 @@ class Categories extends Component {
     return (
       <div className="container">
         <ul className="row">
-          <Products products={filterByCategory} category={category} />
+          <Products products={filterByCategory} category={category} updateTotal={this.updateTotal}/>
         </ul>
       </div>
       
@@ -44,11 +61,11 @@ class Categories extends Component {
                 <h1 className="btn btn-link category-title col-6" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                   {category.name}
                 </h1>
-                <span className="col-6" style={{'float': 'right'}}>(limit: {category.limit})</span>
+                <span className="col-6 limit" style={{'float': 'right', "color": this.state.color}}>{this.state[category.name] || 0}/{category.limit}</span>
               </div>
               <div id="collapseOne" className="collapse show" aria-labelledby="categories" data-parent="#accordion">
               <div className="panel-body">
-                {this.addProducts(category)}  
+                {this.renderProducts(category)}  
               </div>
               </div>
             </div>
--- server/client/src/containers/Products.js
+++ server/client/src/containers/Products.js
@@ -4,19 +4,17 @@ import * as actionTypes from '../actions';
 import '../static/products.css';
 
 class Products extends Component {
-  state = {}
+  state = {
+    sum: 0
+  }
+
 
   //add one item to cart on each click
-  addToCart = (item) => {
-
+  addToCart = (item, updateTotal) => {
     //check to make sure limit in category has not been reached
     if (this.state.length !== 0) {
       const limit = this.props.category.limit
-      let sum = 0
-      for (let key in this.state) {
-        sum += this.state[key]
-      }
-      if (sum >= limit){
+      if (this.state.sum >= limit){
         throw alert (`Limit reached, please remove some ${item.category} items from your cart to add this item`)
       }
       //if item already in state, add one
@@ -24,14 +22,18 @@ class Products extends Component {
         //check to make sure quantity hasn't been reached
         if(this.state[item.name] < item.quantity) {
           const qty = parseInt(this.state[item.name]) + 1
-          this.setState({[item.name] : qty}, () => {
+          let sum = this.state.sum + 1
+          this.setState({[item.name] : qty, sum}, () => {
+            updateTotal(item.category, this.state.sum)
             this.props.updateCart(this.state, this.props.category.name)
           })
         } else {
             throw alert (`Sorry we only have ${item.quantity} ${item.name} in stock. Please add a different item`)
           }
       } else {
-          this.setState({[item.name]: 1}, () => {
+          let sum = this.state.sum + 1
+          this.setState({[item.name]: 1, sum}, () => {
+            updateTotal(item.category, this.state.sum)
             this.props.updateCart(this.state, this.props.category.name)
           })
         }
@@ -39,10 +41,12 @@ class Products extends Component {
   }
 
   //subtract one item from cart on click
-  subtractFromCart = (item) => {
+  subtractFromCart = (item, updateTotal) => {
     if (this.state[item.name] > 0) {
       const qty = parseInt(this.state[item.name]) - 1
-      this.setState({[item.name] : qty}, () => {
+      let sum = this.state.sum - 1
+      this.setState({[item.name] : qty, sum}, () => {
+        updateTotal(item.category, this.state.sum)
         this.props.updateCart(this.state, this.props.category.name)
       })
     } else {
@@ -55,16 +59,18 @@ class Products extends Component {
   render() {
     if (this.props.products.length === 0) {
     }
-    return this.props.products.map (item => {
+    const {products, updateTotal} = this.props
+    return products.map (item => {
     return (
       <li className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2" key={item._id}>
         <div className="card">
           <img className="card-img-top" src={item.image} alt="food item" />
           <h5 className="card-title">{item.name}</h5>
           <div className="qty mt-5">
-            <span className="minus bg-secondary" onClick={() => this.subtractFromCart(item)}>-</span>
-            <input type="number" className="count" name={item.name} value={this.state[item.name]} defaultValue="0" />
-            <span className="plus bg-secondary" onClick={() => this.addToCart(item)}>+</span>
+            <span className="minus bg-secondary" onClick={() => this.subtractFromCart(item, updateTotal)}>-</span>
+            {/* <h3 className="count">{this.state[item.name] || 0}</h3> */}
+            <input type="number" className="count" name={item.name} value={this.state[item.name] || 0} readOnly />
+            <span className="plus bg-secondary" onClick={() => this.addToCart(item, updateTotal)}>+</span>
           </div>
         </div>
       </li>
