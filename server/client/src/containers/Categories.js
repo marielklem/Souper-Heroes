c003ae78a6d9405e0849bf069e687b59ab3e57fe
marielklem <mariel.steinbeiser@gmail.com>
2019-05-12 08:21:57 -0400

    updated styling for products

--- server/client/src/containers/Categories.js
+++ server/client/src/containers/Categories.js
@@ -3,28 +3,41 @@ import { connect } from 'react-redux';
 import * as actionTypes from "../actions";
 import Products from "./Products"
 
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import { Collapse, CardHeader, CardBody, Card } from 'reactstrap';
 
 class Categories extends Component {
-  state = {
-    color: "green"
-  }
+  state = {}
   //on page load, fetch all products
   componentDidMount() {
     this.props.fetchCategories();
     this.props.fetchProducts();
   }
 
+  //toggle each category container open and closed on click
+  
+
+  //update total to auto collapse each card and update total vs limit in each category
   updateTotal = (category, total) => {
-    this.setState({[category]: total}, () => {
+    this.setState(
+      {[category]: {
+        total: total,
+        color: "green",
+        collapse: true
+      }}, () => {
       const category1 = this.props.categories.find( (limit) => {
         return limit.name === category
       })
-      if (this.state[category] >= category1.limit) {
-        this.setState({color: "red"})
-      } else {
-        this.setState({color: "green"})
-      }
+      console.log(this.state)
+      if (this.state[category].total >= category1.limit) {
+        
+        this.setState({
+          [category1.name]: {
+            color: "red",
+            collapse: false,
+            total: total
+          }
+        })
+      } 
     })
   }
 
@@ -37,11 +50,14 @@ class Categories extends Component {
       return (product.category === category.name)
     })
     return (
-      <div className="container">
-        <ul className="row">
-          <Products products={filterByCategory} category={category} updateTotal={this.updateTotal}/>
-        </ul>
-      </div>
+      <CardBody>
+        <div className="container">
+          <ul className="row">
+            <Products products={filterByCategory} category={category} updateTotal={this.updateTotal}/>
+          </ul>
+        </div>
+      </CardBody>
+
       
     )
   }
@@ -53,26 +69,42 @@ class Categories extends Component {
       )
     }
     return this.props.categories.map (category => {
+      if (this.state[category.name]) {
+        console.log(this.state[category.name].color)
+      }
       return (
-        <div key={category._id} className="container">
-          <div id="accordion" style={{'padding': '10px'}} className="row">
-            <div className="card">
-              <div className="card-header" id="categories">
-                <h1 className="btn btn-link category-title col-6" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  {category.name}
+        <div className="container">
+          <Card>
+            <CardHeader key={category._id} onClick={this.toggleCategory}>
+              <h1 className="category-title col-6">
+                    {category.name}
                 </h1>
-                <span className="col-6 limit" style={{'float': 'right', "color": this.state.color}}>{this.state[category.name] || 0}/{category.limit}</span>
-              </div>
-              <div id="collapseOne" className="collapse show" aria-labelledby="categories" data-parent="#accordion">
-              <div className="panel-body">
-                {this.renderProducts(category)}  
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                <span className="col-6 limit" style={{"color": (this.state[category.name] ? this.state[category.name].color : "green") }}>{ this.state[category.name] ? this.state[category.name].total : 0}/{category.limit}</span>
+            </CardHeader>
+            <Collapse isOpen={this.state[category.name] ? this.state[category.name].collapse : true}>
 
+              {this.renderProducts(category)} 
 
+              </Collapse>
+          </Card>
+      </div>
+        // <div key={category._id} className="container">
+        //   <div id="accordion" style={{'padding': '10px'}} className="row" >
+        //     <div className="card" isOpen={this.state.collapse}>
+        //       <div className="card-header" id="categories">
+                // <h1 className="btn btn-link category-title col-6" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                //   {category.name}
+                // </h1>
+                // <span className="col-6 limit" style={{'float': 'right', "color": this.state.color}}>{this.state[category.name] || 0}/{category.limit}</span>
+        //       </div>
+        //       <div id="collapseOne" className="collapse show" aria-labelledby="categories" data-parent="#accordion">
+        //       <div className="panel-body">
+        //         {this.renderProducts(category)}  
+        //       </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
       )
     })
   }
