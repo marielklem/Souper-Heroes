1642a97438d0c9227679f334f567af386740fd13
marielklem <mariel.steinbeiser@gmail.com>
2019-05-13 16:12:34 -0400

    fixed hunger logo

--- server/client/src/containers/Welcome.js
+++ server/client/src/containers/Welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionTypes from "../actions";
 import grapes from "../static/grapes.jpg"
-import feed from "../static/tin-cans.png"
+import feed from "../static/network.png"
 import {Link} from 'react-router-dom';
 
 class Welcome extends Component {
